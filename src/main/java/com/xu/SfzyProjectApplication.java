21023a628294ade2c7ad48d95cf0a63144d9cbbf
xu <2265035472@qq.com>
2026-07-09 12:32:32 +0800

    项目初始化

---
+++ src/main/java/com/xu/SfzyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfzyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfzyProjectApplication.class, args);
+    }
+
+}
