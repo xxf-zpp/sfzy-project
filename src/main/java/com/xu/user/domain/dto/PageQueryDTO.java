33a6d5dc9541fc45f5527478e893b88f2aadbc4b
xu <2265035472@qq.com>
2026-07-15 18:44:55 +0800

    分页dto完善

--- src/main/java/com/xu/user/domain/dto/PageQueryDTO.java
+++ src/main/java/com/xu/user/domain/dto/PageQueryDTO.java
@@ -7,4 +7,11 @@ public class PageQueryDTO {
     private Integer pageSize = 10;
     private Integer PageNum = 1;
     private Integer status;
+
+    //筛选咨询师
+    private String address;
+    private String goodAT;
+    private String level;
+    private String gender;
+    private String typeName;
 }
